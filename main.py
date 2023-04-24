a38d275e6586dfe5725fdb197b8b31162dd8e299
Ba1an <karolina_karo_balan@mail.ru>
2023-04-24 16:23:09 +0700

    little change

--- main.py
+++ main.py
@@ -1,84 +1,78 @@
+import ru_local as ru
 s = 0
 
 print('Здравствуйсте! Ответьте на следующие вопросы для расчёта НДФЛ.' )
 r = input('Вы находились 183 дня в РФ за последние 12 месяцев или являютесь российским военным, служащим за границей,'
           'или сотрудником органов власти, командированным за рубеж.? ')
 if r.lower()=='да':
-    s = 0
-
-    print('Здравствуйсте! Ответьте на следующие вопросы для расчёта НДФЛ.')
-    r = input(
-        'Вы находились 183 дня в РФ за последние 12 месяцев или являютесь российским военным, служащим за границей,'
-        'или сотрудником органов власти, командированным за рубеж.? ')
-    if r.lower() == 'да':
-        asset = input(ru.asset_q1)
-        if asset.lower() == 'да':
-            family = input(ru.asset_q2)
-            exemption = input(ru.asset_q3)
-            how_much = input(ru.asset_q4)
-            how_long = input(ru.asset_q5)
-            gain = input(ru.asset_q6)
-            if family.lower() == 'да' or exemption.lower() == 'да' or how_much.lower() == 'да' or how_long.lower() == 'да' or gain.lower() == 'да':
-                asset = 0
-            else:
-                tax_base = int(input(ru.tax_base_asset))
-                asset = tax_base * 0.13
-        labor = input(ru.labor_q1)
-        if labor.lower() == 'да':
-            tax_base = int(input(ru.tax_base_labor))
-            if tax_base < 5000000:
-                labor = tax_base * 0.13
-            else:
-                labor = tax_base * 0.15
-        CLC = input(CLC_q1)
-        if CLC.lower() == 'да':
-            tax_base = int(input(tax_base_CLC))
-            if tax_base < 5000000:
-                CLC = tax_base * 0.13
-            else:
-                CLC = tax_base * 0.15
-        divident = input(divident_q1)
-        if divident.lower() == 'да':
-            tax_base = int(input(tax_base_divident))
-            divident = tax_base * 0.13
-        others = input(others_q1)
-        if others.lower() == 'да':
-            tax_base = int(input(tax_base_other))
-            others = tax_base * 0.30
-        persantage = input('Получали ли вы доход по облигациям с ипотечным покрытием, приобретенным до 1 января 2007 года?')
-        if persantage.lower() == 'да':
-            persantage = int(input('Введите ваш доход по облигациям с ипотечным покрытием.'))
-            persantage *= 0.09
+    asset = input(ru.asset_q1)
+    if asset.lower() == 'да':
+        family = input(ru.asset_q2)
+        exemption = input(ru.asset_q3)
+        how_much = input(ru.asset_q4)
+        how_long = input(ru.asset_q5)
+        gain = input(ru.asset_q6)
+        if family.lower() == 'да' or exemption.lower() == 'да' or how_much.lower() == 'да' or how_long.lower() == 'да' or gain.lower() == 'да':
+            asset = 0
         else:
-            persantage_1 = input('Получали ли вы доход как учредитель доверительного управления ипотечным покрытием по сертификатам,'
+            tax_base = int(input(ru.tax_base_asset))
+            asset = tax_base * 0.13
+    labor = input(ru.labor_q1)
+    if labor.lower() == 'да':
+        tax_base = int(input(ru.tax_base_labor))
+        if tax_base < 5000000:
+            labor = tax_base * 0.13
+        else:
+            labor = tax_base * 0.15
+    CLC = input(ru.CLC_q1)
+    if CLC.lower() == 'да':
+        tax_base = int(input(ru.tax_base_CLC))
+        if tax_base < 5000000:
+            CLC = tax_base * 0.13
+        else:
+            CLC = tax_base * 0.15
+    divident = input(ru.divident_q1)
+    if divident.lower() == 'да':
+        tax_base = int(input(ru.tax_base_divident))
+        divident = tax_base * 0.13
+    others = input(ru.others_q1)
+    if others.lower() == 'да':
+        tax_base = int(input(ru.tax_base_other))
+        others = tax_base * 0.30
+    persantage = input('Получали ли вы доход по облигациям с ипотечным покрытием, приобретенным до 1 января 2007 года?')
+    if persantage.lower() == 'да':
+        persantage = int(input('Введите ваш доход по облигациям с ипотечным покрытием.'))
+        persantage *= 0.09
+    else:
+        persantage_1 = input('Получали ли вы доход как учредитель доверительного управления ипотечным покрытием по сертификатам,'
                 'приобретенным до 1 января 2007 года? ')
-            if persantage_1.lower() == 'да':
-                persantage = int(input('Введите ваш доход как учредителя доверительного управления ипотечным покрытием.'))
-                persantage *= 0.09
-        gain = input('Получали ли вы выигрыши/призы в конкурсах, играх и других мероприятиях в целях рекламы, в части превышения установленных размеров?')
-        if gain.lower() == 'да':
-            gain = int(input('Введите стоимость выиграша/приза.'))
-            gain *= 0.35
-        else:
-            gain = 0
-        others = input('Платили ли вы за использование денежных средств членов кредитного потребительского кооператива, в части превышения установленных размеров?')
-        if others.lower() == 'да':
-            others = int(input('Введите сумму за использование.'))
-            others *=0.35
-        else:
-            others = 0
-        deposit = input('Превышал ли ваш доход по вкладам в банках установленных размеров?')
-        if deposit.lower() == 'да':
-            deposit = int(input('Введите ваш доход по вкладам в банках.'))
-            deposit *=0.35
-        else:
-            deposit = 0
-        cooperative = input('Выплачивали ли вы проценты за использование сельскохозяйственным кредитным потребительским кооперативом средств, в части превышения установленных размеров?')
-        if cooperative.lower() == 'да':
-            cooperative = int(input('Введите сумму выплаченных процентов.'))
-            cooperative *=0.35
-        else:
-            cooperative = 0
+        if persantage_1.lower() == 'да':
+            persantage = int(input('Введите ваш доход как учредителя доверительного управления ипотечным покрытием.'))
+            persantage *= 0.09
+    gain = input('Получали ли вы выигрыши/призы в конкурсах, играх и других мероприятиях в целях рекламы, в части превышения установленных размеров?')
+    if gain.lower() == 'да':
+        gain = int(input('Введите стоимость выиграша/приза.'))
+        gain *= 0.35
+    else:
+        gain = 0
+    others = input('Платили ли вы за использование денежных средств членов кредитного потребительского кооператива, в части превышения установленных размеров?')
+    if others.lower() == 'да':
+        others = int(input('Введите сумму за использование.'))
+        others *=0.35
+    else:
+        others = 0
+    deposit = input('Превышал ли ваш доход по вкладам в банках установленных размеров?')
+    if deposit.lower() == 'да':
+        deposit = int(input('Введите ваш доход по вкладам в банках.'))
+        deposit *=0.35
+    else:
+        deposit = 0
+    cooperative = input('Выплачивали ли вы проценты за использование сельскохозяйственным кредитным потребительским кооперативом средств, в части превышения установленных размеров?')
+    if cooperative.lower() == 'да':
+        cooperative = int(input('Введите сумму выплаченных процентов.'))
+        cooperative *=0.35
+    else:
+        cooperative = 0
 else:
     asset = input('Получали ли вы доход от продажи имущества? ')
     if asset.lower() == 'да':
